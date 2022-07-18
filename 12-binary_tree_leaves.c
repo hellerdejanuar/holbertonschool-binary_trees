887dbeb8a706e28ceb34feede204aeab36e365b0
hellerdejanuar <hellerdejanuar@gmail.com>
2022-07-18 12:25:08 -0700

    12 add

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - counts leaves
+ * @tree: tree
+ * Return: 0 if failure, pointer to new_node on Success
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree == NULL)
+		return (0);
+
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left)
+		leaves = binary_tree_leaves(tree->left);
+	if (tree->right)
+		leaves += binary_tree_leaves(tree->right);
+
+
+	return (leaves);
+}
