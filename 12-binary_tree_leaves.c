76fac0ec736d4310f490e3876f4e9e64c15e9b55
hellerdejanuar <hellerdejanuar@gmail.com>
2022-07-18 12:53:47 -0700

    13 upd

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_leaves - counts leaves
  * @tree: tree
- * Return: 0 if failure, pointer to new_node on Success
+ * Return: 0 if tree is NULL, <leaves count> on success
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
