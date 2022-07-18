5633526f8d314ab12cd584775a925ecf820deb46
Vinill <danimuller758@gmail.com>
2022-07-18 15:36:17 +0000

    fixed betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,20 +1,19 @@
 #include "binary_trees.h"
 /**
  * binary_tree_delete - function that deletes an entire binary tree
- * @parent: parent of the node to create
- * @value: value
+ * @tree: tree
  * Return: NULL if failure, pointer to new_node on Success
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
+	if (tree == NULL)
+		return;
 
-    if (tree->left != NULL)
-        binary_tree_delete(tree->left);
+	if (tree->left != NULL)
+		binary_tree_delete(tree->left);
 
-    if (tree->right != NULL)
-        binary_tree_delete(tree->right);
+	if (tree->right != NULL)
+		binary_tree_delete(tree->right);
 
 free(tree);
 return;
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,15 +1,13 @@
 #include "binary_trees.h"
 /**
- * binary_tree_delete - function that deletes an entire binary tree
- * @parent: parent of the node to create
- * @value: value
+ * binary_tree_is_leaf - function that deletes an entire binary tree
+ * @node: node
  * Return: NULL if failure, pointer to new_node on Success
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL || node->left != NULL || node->right != NULL)
-        return 0;
+	if (node == NULL || node->left != NULL || node->right != NULL)
+		return (0);
 
-    return 1;
-
+	return (1);
 }
