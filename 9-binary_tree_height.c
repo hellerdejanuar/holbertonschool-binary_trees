13682744550b283090d4e444c4cca689ef521792
hellerdejanuar <hellerdejanuar@gmail.com>
2022-07-19 10:01:44 -0700

    9 fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,15 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height1, height2;
+	size_t height1 = 0, height2 = 0;
 
-	if (!tree || (!tree->left && !tree->right))
+	if (!tree)
 		return (0);
 
-	{
-		height1 = binary_tree_height(tree->left) + 1;
-		height2 = binary_tree_height(tree->right) + 1;
-	}
+	height1 = binary_tree_height(tree->left) + 1;
+	height2 = binary_tree_height(tree->right) + 1;
 
 	if (height1 > height2)
 		return (height1);
