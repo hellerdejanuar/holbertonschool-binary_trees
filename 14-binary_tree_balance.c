4fe9009b9865be2c30b022c0784235f863331240
hellerdejanuar <hellerdejanuar@gmail.com>
2022-07-19 09:51:56 -0700

    14 add

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,25 +1,46 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_nodes - counts nodes downstream w/ at least one child
- * @tree: tree
- * Return: 0 if tree is NULL, <node count> on success
+ * binary_tree_balance - return the balance factor of the tre
+ * @tree: pointer to te tree node
+ * Return: Positive int if tree balanced left, negative if right
  */
-size_t binary_tree_nodes(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t nodes = 0;
+	int left_h = 0, right_h = 0;
 
-	if (tree == NULL)
-		return (0);
-
-
-	if (tree->left == NULL && tree->right == NULL)
+	if (!tree)
 		return (0);
 
 	if (tree->left)
-		nodes = binary_tree_nodes(tree->left);
+		left_h = binary_tree_height(tree->left);
 	if (tree->right)
-		nodes += binary_tree_nodes(tree->right);
+		right_h = binary_tree_height(tree->right);
 
-
-	return (nodes + 1);
+	return (left_h - right_h);
 }
+
+
+#include "binary_trees.h"
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to te tree node
+ * Return: Address of thenew element or NULL if it failed
+ *
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height1 = 0, height2 = 0;
+
+	if (!tree)
+		return (0);
+
+	height1 = binary_tree_height(tree->left) + 1;
+	height2 = binary_tree_height(tree->right) + 1;
+
+	if (height1 > height2)
+		return (height1);
+	else
+		return (height2);
+}
+
--- binary_trees.h
+++ binary_trees.h
@@ -2,7 +2,7 @@
 #define BINARY_TREES_H
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -36,4 +36,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
 #endif
