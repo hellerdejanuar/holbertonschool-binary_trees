24fe8b1c8f769c2b4012400ad0a7b4fb41e22e99
hellerdejanuar <hellerdejanuar@gmail.com>
2022-07-19 12:55:57 -0700

    18 working

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,51 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle (sibling of paret) of a node
+ * @node: pointer to the node
+ * Return: pointer to sibling of the node. NULL if fail or no sibling.
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+
+	if (node->parent)
+		return (binary_tree_sibling(node->parent));
+
+	return (NULL);
+}
+
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to the node
+ * Return: pointer to sibling of the node. NULL if fail or no sibling.
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+
+	if (node->parent)
+	{
+		if (node->parent->left == node)
+		{
+			if (node->parent->right)
+				return (node->parent->right);
+			else
+				return (NULL);
+		}
+
+		else if (node->parent->right == node)
+		{
+			if (node->parent->left)
+				return (node->parent->left);
+			else
+				return (NULL);
+		}
+	}
+
+	return (NULL);
+}
