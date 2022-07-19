a401676e2cfae9a8705a90682bd0dfb2a7b2905c
hellerdejanuar <hellerdejanuar@gmail.com>
2022-07-19 12:50:34 -0700

    17 working

--- binary_trees.h
+++ binary_trees.h
@@ -39,4 +39,5 @@ size_t binary_tree_height(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int is_perfect_recursion(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 #endif
